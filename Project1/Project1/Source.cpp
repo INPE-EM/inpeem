c8d24fefe3d62bbd307d52b8e21243b9ea51949e
Diego Melo <melo.diego@gmail.com>
2016-12-09 18:17:02 +0000

    Update on help files

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -6,8 +6,8 @@ using namespace INPEEM;
 [STAThreadAttribute]
 int main(void)
 {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
+	//Application::EnableVisualStyles();
+	//Application::SetCompatibleTextRenderingDefault(false);
 	INPEEM::MyForm^ myForm1 = gcnew INPEEM::MyForm();
 	Application::Run(myForm1);
 }
