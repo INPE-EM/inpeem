1bd3c1e9e9b0bfd34a726b949e13621b75ce7610
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2020-10-26 14:27:12 -0300

    small changes.

--- build/scripts/win/inpeem_conf.bat
+++ build/scripts/win/inpeem_conf.bat
@@ -1,33 +1,29 @@
-if "%_INPEEM_GIT_DIR%" == "" (
-	set _INPEEM_GIT_DIR=..\..\git\inpeem
-)
-
 if "%_INPEEM_BUILD_DIR%" == "" (
 	set _INPEEM_BUILD_DIR=%CD%\build
 )
 
 if "%_INPEEM_INSTALL_DIR%" == "" (
-  set _INPEEM_INSTALL_DIR=%CD%\install
+	set _INPEEM_INSTALL_DIR=%CD%\install
 )
 
 if "%_INPEEM_BUILD_TYPE%" == "" (
-  set _INPEEM_BUILD_TYPE=Release
+	set _INPEEM_BUILD_TYPE=Release
 )
 
 if "%_INPEEM_BUILD_AS_BUNDLE%" == "" (
-  set _INPEEM_BUILD_AS_BUNDLE=ON
+	set _INPEEM_BUILD_AS_BUNDLE=ON
 )
 
 if "%_INPEEM_CREATE_INSTALLER%" == "" (
-  set _INPEEM_CREATE_INSTALLER=ON
+	set _INPEEM_CREATE_INSTALLER=ON
 )
 
 if "%_INPEEM_TERRAME_DIR%" == "" (
-  set _INPEEM_TERRAME_DIR=D:/inpe-em/3rdparty/terrame
+	set _INPEEM_TERRAME_DIR=D:/inpe-em/3rdparty/terrame
 )
 
 if "%_Qt5_DIR%" == "" (
-  set _Qt5_DIR=C:\Qt\5.14\5.14.2\msvc2017_64
+	set _Qt5_DIR=C:\Qt\5.10.1\msvc2017_64
 )
 
 set _INPEEM_SOLUTION_DIR=%CD%
@@ -35,9 +31,11 @@ set _INPEEM_SOLUTION_DIR=%CD%
 mkdir %_INPEEM_BUILD_DIR%
 cd %_INPEEM_BUILD_DIR%
 
-xcopy /Y %_INPEEM_GIT_DIR%\build\scripts\win\inpeem-conf.cmake %_INPEEM_BUILD_DIR%
+if "%_INPEEM_GIT_DIR%" == "" (
+	set _INPEEM_GIT_DIR=..\..\git\inpeem
+)
 
-cmake -G "Visual Studio 15 2017 Win64" -C inpeem-conf.cmake %_INPEEM_GIT_DIR%\build\cmake
+cmake -G "Visual Studio 15 2017 Win64" -C %_INPEEM_GIT_DIR%\build\cmake\inpeem_build_conf.cmake %_INPEEM_GIT_DIR%\build\cmake
 cmake --build . --target INSTALL --config %_INPEEM_BUILD_TYPE%
 
 if "%_INPEEM_CREATE_INSTALLER%" == "ON" (
